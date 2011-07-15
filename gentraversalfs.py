44570b0f3f3601b5d4e8eca639cdf59d1b0b16eb
Philipp Hagemeister <phihag@phihag.de>
2011-07-15 10:09:00 +0200

    Refactor gentraversalfs

--- gentraversalfs.py
+++ gentraversalfs.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 
 import json,os,sys,mimetypes,contextlib,base64,re,bz2,tarfile
-import pickle
 
 def listFilesystem(root):
 	""" Yields entries in the form (virtual filename, children (None if an element), file handle (None if a directory)) """
@@ -50,21 +49,16 @@ def parseFiles(iterFiles):
 			fdata['type'] = mimetypes.guess_type(vfn)[0]
 			with contextlib.closing(fileh) as f:
 				content = f.read()
-				fdata['blob_b64'] = base64.b64encode(content)
+				fdata['blob_b64'] = base64.b64encode(content).decode('ascii')
 		res[vfn] = fdata
 	return res
 
-def main():
-	if len(sys.argv) != 2:
-		print('Usage: ' + sys.argv[0] + ' [root]')
-		print('root can be a directory or a tarfile.')
-		sys.exit(101)
-	fsroot = sys.argv[1]
+def genFilesRaw(source):
 	try:
-		iterFiles = listFilesystem(fsroot)
+		iterFiles = listFilesystem(source)
 		fs = parseFiles(iterFiles)
 	except ValueError:
-		iterFiles = listTar(fsroot)
+		iterFiles = listTar(source)
 		fs = parseFiles(iterFiles)
 	if not '/var/www/img' in fs:
 		raise ValueError('Filesystem is missing image directory')
@@ -72,14 +66,22 @@ def main():
 		raise ValueError('Image directory empty')
 	if not '' in fs:
 		raise ValueError('Filesystem is missing root directory')
-	s = json.dumps(fs)
-	s64 = base64.b64encode(bz2.compress(s))
+	raw = json.dumps(fs).encode('utf-8')
+	raw = bz2.compress(raw)
+	return base64.b64encode(raw).decode('ascii')
+
+def main():
+	if len(sys.argv) != 2:
+		print('Usage: ' + sys.argv[0] + ' [root]')
+		print('root can be a directory or a tarfile.')
+		sys.exit(101)
+	filesRaw = genFilesRaw(sys.argv[1])
 
 	scriptfn = os.path.join(os.path.dirname(__file__), 'vulnsrv.py')
 	with contextlib.closing(open(scriptfn, 'r+')) as scriptf:
 		code = scriptf.read()
 		p = re.compile('_FILES_RAW = "[^"]*"')
-		code = p.sub('_FILES_RAW = "' + s64 + '"', code)
+		code = p.sub('_FILES_RAW = "' + filesRaw + '"', code)
 
 		scriptf.seek(0)
 		scriptf.truncate(0)
