946c78e2b1e9fd239a288ef0e95c1add395c6100
Philipp Hagemeister <phihag@phihag.de>
2013-06-11 17:11:00 +0200

    Allow multiple filesystem roots

--- set_content.py
+++ set_content.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json,os,mimetypes,contextlib,base64,re,bz2,tarfile
+import base64
+import bz2
+import contextlib
+import itertools
+import json
+import mimetypes
+import os
+import re
+import tarfile
 from optparse import OptionParser
+
+def listFiles(fn):
+	if os.path.isdir(fn):
+		return listFilesystem(fn)
+	else:
+		return listTar(fn)
 
 def listFilesystem(root):
 	""" Yields entries in the form (virtual filename, children (None if an element), file handle (None if a directory)) """
-	if not os.path.isdir(root):
-		raise ValueError('Root is not a directory')
 	visit = [(root, '')]
 	while len(visit) > 0:
 		r,v = visit.pop(0)
@@ -54,13 +66,8 @@ def parseFiles(iterFiles):
 		res[vfn] = fdata
 	return res
 
-def genFilesRaw(source):
-	try:
-		iterFiles = listFilesystem(source)
-		fs = parseFiles(iterFiles)
-	except ValueError:
-		iterFiles = listTar(source)
-		fs = parseFiles(iterFiles)
+def genFilesRaw(iterFiles):
+	fs = parseFiles(iterFiles)
 	if not '/var/www/img' in fs:
 		raise ValueError('Filesystem is missing image directory')
 	if len(fs['/var/www/img']['content']) == 0:
@@ -89,21 +96,26 @@ def replaceConstant(code, constant, rawVal):
 
 def main():
 	op = OptionParser()
-	op.add_option('-t', '--traversal-root', dest='traversalroot', help='Directory or tar file containing the filesystem presented in the path traversal task')
-	op.add_option('-f', '--favicon', dest='favicon', help='The favicon file (browser tab/window icon)')
-	op.add_option('-d', '--dbdata-file', dest='dbdata_file', help='Write the initial database data content from the specified, compressed base16-encoded file')
+	op.add_option('-t', '--traversal-root', dest='traversalroots',
+				  action='append',
+				  help='Directory or tar file containing the filesystem presented in the path traversal task (can be specified multiple times)'),
+	op.add_option('-f', '--favicon', dest='favicon',
+		          help='The favicon file (browser tab/window icon)')
+	op.add_option('-d', '--dbdata-file', dest='dbdata_file',
+		          help='Write the initial database data content from the specified, compressed base16-encoded file')
 	options,args = op.parse_args()
 
 	if len(args) != 0:
 		op.error('incorrect number of arguments, use -t and/or -f')
-	if options.traversalroot is None and options.favicon is None and options.dbdata_file is None:
+	if options.traversalroots is None and options.favicon is None and options.dbdata_file is None:
 		op.error('Please supply at least one of -t, -f, or -d')
-	
+
 	scriptfn = os.path.join(os.path.dirname(__file__), 'vulnsrv.py')
 	with contextlib.closing(open(scriptfn, 'r+')) as scriptf:
 		code = scriptf.read()
-		if options.traversalroot is not None:
-			filesRaw = genFilesRaw(options.traversalroot)
+		if options.traversalroots is not None:
+			fileList = itertools.chain(* map(listFiles, options.traversalroots))
+			filesRaw = genFilesRaw(fileList)
 			code = replaceConstant(code, '_FILES_RAW', filesRaw)
 		if options.favicon is not None:
 			faviconRaw = genFaviconRaw(options.favicon)
