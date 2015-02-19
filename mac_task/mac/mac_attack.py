2d3c0cfcef3fc9714ac8453f458f4e320bb8ac47
Philipp Hagemeister <phihag@phihag.de>
2015-02-19 16:36:01 +0100

    [mac_attack.py] PEP8

--- mac_task/mac/mac_attack.py
+++ mac_task/mac/mac_attack.py
@@ -5,12 +5,12 @@ SECRETLEN = 32
 
 
 import subprocess
-import sys
+
 
 # Compatibility for Python 2 and 3
 try:
     from urllib.request import HTTPCookieProcessor, build_opener, Request, urlopen
-except ImportError: # Python < 3
+except ImportError:  # Python < 3
     from urllib2 import HTTPCookieProcessor, build_opener, Request, urlopen
 try:
     from urllib.parse import urlencode
@@ -25,7 +25,7 @@ if not hasattr(str, 'partition'):
         if pos < 0:
             return (self, '', '')
         else:
-            return (self[:pos], sep, self[pos+len(sep):])
+            return (self[:pos], sep, self[pos + len(sep):])
 else:
     _partition = str.partition
 
@@ -35,15 +35,15 @@ if not hasattr(subprocess, 'check_output'):
         kwargs = dict(kwargs)
         kwargs['stdout'] = subprocess.PIPE
         sp = subprocess.Popen(*args, **kwargs)
-        stdout,_ = sp.communicate()
+        stdout, _ = sp.communicate()
         if sp.returncode != 0:
             raise subprocess.CalledProcessError('Command returned non-zero exit status %r' % sp.returncode)
         return stdout
+
     class _CalledProcessError(Exception):
         pass
     subprocess.CalledProcessError = _CalledProcessError
     subprocess.check_output = _check_output
-
 
 
 def _getCookie(cookiecp, key):
@@ -53,11 +53,12 @@ def _getCookie(cookiecp, key):
             return c.value.strip('"')
     raise ValueError('Cookie not found')
 
+
 def getGuestCookie():
     cookiecp = HTTPCookieProcessor()
     opener = build_opener(cookiecp)
 
-    r = opener.open(URL)
+    opener.open(URL)
     token = _getCookie(cookiecp, 'sessionID')
 
     data = {'csrfToken': token}
@@ -65,11 +66,12 @@ def getGuestCookie():
     headers = {'Cookie': 'sessionID=' + token}
 
     req = Request(URL + 'login', data_enc.encode('ascii'), headers)
-    r = opener.open(req)
+    opener.open(req)
     return _getCookie(cookiecp, 'mac_session')
 
+
 def main():
-    h,_,val = _partition(getGuestCookie(), '!')
+    h, _, val = _partition(getGuestCookie(), '!')
     inject = b('&user=admin')
 
     exploit = subprocess.check_output([
